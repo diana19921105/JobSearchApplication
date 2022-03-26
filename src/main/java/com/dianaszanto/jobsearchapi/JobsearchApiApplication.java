188253bc829288b00cc09238f9b1ce2b16ca419a
Diána Szántó <szanto.diana.1992@gmail.com>
2022-03-26 17:20:17 +0100

    initial commit

---
+++ src/main/java/com/dianaszanto/jobsearchapi/JobsearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.dianaszanto.jobsearchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsearchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobsearchApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dianaszanto/jobsearchapi/JobsearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dianaszanto.jobsearchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsearchApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
