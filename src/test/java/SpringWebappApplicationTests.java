081d9444432a5fbf64660d1fe94bed7efb2a2764
Diána Szántó <szanto.diana.1992@gmail.com>
2022-04-03 00:04:58 +0200

    test classes were extended

--- src/test/java/SpringWebappApplicationTests.java
+++ src/test/java/SpringWebappApplicationTests.java
@@ -1,3 +1,4 @@
+import com.dianaszanto.jobsearchapi.controller.ClientControllerUnitTest;
 import com.dianaszanto.jobsearchapi.service.impl.JobServiceImplTest;
 import com.dianaszanto.jobsearchapi.unit.ClientServiceUnitTest;
 import org.junit.Test;
@@ -6,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {ClientServiceUnitTest.class, JobServiceImplTest.class})
+@SpringBootTest(classes = {ClientServiceUnitTest.class, JobServiceImplTest.class, ClientControllerUnitTest.class})
 public class SpringWebappApplicationTests {
 
     @Test
--- src/test/java/com/dianaszanto/jobsearchapi/service/impl/JobServiceImplTest.java
+++ src/test/java/com/dianaszanto/jobsearchapi/service/impl/JobServiceImplTest.java
@@ -8,6 +8,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -29,6 +35,26 @@ public class JobServiceImplTest {
 
         jobService.save(job);
 
-        verify(jobRepository,times(1)).save(any());
+        verify(jobRepository, times(1)).save(any());
+    }
+
+    @Test
+    public void givenFindAllByKeyWord_whenKeyWordIsNull_thenReturnAll() throws MalformedURLException {
+        List<Job> allJobs = new ArrayList<>();
+        allJobs.add(new Job("title", "location", new URL("http://localhost:8080/positions")));
+        allJobs.add(new Job("title1", "location1", new URL("http://localhost:8080/positions1")));
+
+        when(jobRepository.findAll()).thenReturn(allJobs);
+
+        List<Job> expected = jobService.findAllByKeyWord(null);
+
+        assertEquals(expected.get(0).getTitle(), allJobs.get(0).getTitle());
+        assertEquals(expected.get(0).getLocation(), allJobs.get(0).getLocation());
+        assertEquals(expected.get(0).getUrl().toString(), allJobs.get(0).getUrl().toString());
+        assertEquals(expected.get(1).getTitle(), allJobs.get(1).getTitle());
+        assertEquals(expected.get(1).getLocation(), allJobs.get(1).getLocation());
+        assertEquals(expected.get(1).getUrl().toString(), allJobs.get(1).getUrl().toString());
+
+        verify(jobRepository, times(1)).findAll();
     }
 }
